797feb7935188f27861a1795b17dbca855408d59
heloísa <110057779+helouuu@users.noreply.github.com>
2022-09-28 17:44:54 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,2 +1,19 @@
-console.log("Alô Mundo...");
-alert("Alô Mundo...");
+//console.log("Alô Mundo...");
+//alert("Alô Mundo...");
+
+let tabuada = 2;
+
+function meChame(){
+  document.write(tabuada + " x 1 = " + (tabuada*1)) +" < br>");
+  document.write(tabuada + " x 2 = " + (tabuada*2)) +" < br>");
+  document.write(tabuada + " x 3 = " + (tabuada*3)) +" < br>");
+  document.write(tabuada + " x 4 = " + (tabuada*4)) +" < br>");
+  document.write(tabuada + " x 5 = " + (tabuada*5)) +" < br>");
+  document.write(tabuada + " x 6 = " + (tabuada*6)) +" < br>");
+  document.write(tabuada + " x 7 = " + (tabuada*7)) +" < br>");
+  document.write(tabuada + " x 8 = " + (tabuada*8)) +" < br>");
+  document.write(tabuada + " x 9 = " + (tabuada*9)) +" < br>");
+  document.write(tabuada + " x 10 = " + (tabuada*10)) +" < br>");
+    
+}
+
