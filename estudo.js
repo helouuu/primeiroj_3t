3f418f260c1c96d9865d004c0c3bc2f7aa54c7e5
heloísa <110057779+helouuu@users.noreply.github.com>
2022-10-05 17:39:09 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,6 +2,7 @@
 // alert("Alerta: Alô mundo...");
 let tabuada = 4;
 
+
 function meChame(){
    document.write("Tabuado do " + tabuada + "<br>");
    document.write(tabuada + " x 1 = " + (tabuada*1) +"<br>");
@@ -15,22 +16,28 @@ function meChame(){
    document.write(tabuada + " x 9 = " + (tabuada*9) +"<br>");
    document.write(tabuada + " x 10 = " + (tabuada*10) +"<br>");
 }
- 
 
 
- function minhaTabuada(){
-    for(var i=1; i <= 10; i++){
+function minhaTabuada(){
+   for(var i=1; i <= 10; i++){
 
-       for(var j = 1; <=10; j++){
-         document.write(i + " x  " + j + " = " + (i*j) +"<br>");
+      for(var j = 1; j <=10; j++){
+         document.write(i + " x " + j + " = " + (i*j) +"<br>");
       }
 
       document.write("<br><br>");
-    }
- }
+   }
+}
 
-  function quadrado (){
-     for(var i = 2; i < 21; i++){
-        document.write("O quadrado de " + i + " é " + (i*i) + "<br>"
-     }
-  }
+function quadrado(){
+   for(var i = 2; i < 21; i++){
+      document.write("O quadrado de " + i + " é " + (i*i) + "<br>");
+   }
+}
+
+function total(){
+   let val = document.getElementById("valor").value;
+   let ju = document.getElementById("juros").value;
+   let resultado = (val * (1+ (ju/100)));
+  document.write("o resultado é " + resultado);
+}
